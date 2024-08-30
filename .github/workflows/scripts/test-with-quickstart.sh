cda784209b22748e7185e74a883f7908d3e7d552
MK Software <mk_software_pl@protonmail.com>
2024-08-30 11:13:27 +0200

    wip

---
+++ .github/workflows/scripts/test-with-quickstart.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -ex
+
+cd "$(dirname "$0")"
+
+cd ../../../
+EXAMPLES_LIBABRY_BUILD_TEMP_VERSION=$(uuidgen | tr -d '-' | tr 'A-Z' 'a-z' | cut -c1-10)
+echo "1. Building Scenario Examples Library image. Version: ${EXAMPLES_LIBABRY_BUILD_TEMP_VERSION}..."
+docker buildx build --tag touk/nussknacker-example-scenarios-library:"$EXAMPLES_LIBABRY_BUILD_TEMP_VERSION" .
+
+echo "2. Checking out Nu Quickstart..."
+cd .github/workflows/scripts
+rm -rf nussknacker-quickstart
+git clone https://github.com/TouK/nussknacker-quickstart.git
+cd nussknacker-quickstart
+git checkout -b staging # TODO: change to main when Nu 1.17 is released
+
+echo "3. Setting proper Scenario Examples Library image version..."
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' "s|touk/nussknacker-example-scenarios-library:latest|touk/nussknacker-example-scenarios-library:${EXAMPLES_LIBABRY_BUILD_TEMP_VERSION}|g" docker-compose.yml
+else
+  sed -i "s|touk/nussknacker-example-scenarios-library:latest|touk/nussknacker-example-scenarios-library:${EXAMPLES_LIBABRY_BUILD_TEMP_VERSION}|g" docker-compose.yml
+fi
+
+on_error() {
+  docker compose logs 
+}
+
+on_exit() {
+  ./stop-and-clean.sh
+}
+
+trap on_error ERR
+trap on_exit EXIT
+
+./start.sh
