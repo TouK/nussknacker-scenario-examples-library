8755f77833879240ff989a036f2c1e674147c064
MK Software <mk_software_pl@protonmail.com>
2025-04-04 16:26:16 +0200

    fix

--- scenario-examples-bootstrapper/setup/nu/customize-nu-configuration.sh
+++ scenario-examples-bootstrapper/setup/nu/customize-nu-configuration.sh
@@ -69,7 +69,7 @@ done
 if ! ../../utils/nu/reload-configuration.sh; then
   RELOAD_EXIT_CODE=$?
   echo "Failed to reload configuration (exit code: $RELOAD_EXIT_CODE). Cleaning up..."
-  #cleanup_nu_configuration
+  cleanup_nu_configuration
   exit $RELOAD_EXIT_CODE
 fi
 
