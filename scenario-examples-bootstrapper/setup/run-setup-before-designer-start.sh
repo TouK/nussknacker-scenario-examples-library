5c3e425a4e3e69d7d7afc02349766b3592fb6dcf
Arek Burdach <arek.burdach@gmail.com>
2025-10-15 15:24:07 +0200

    Removed "dependant" word

--- scenario-examples-bootstrapper/setup/run-setup-before-designer-start.sh
+++ scenario-examples-bootstrapper/setup/run-setup-before-designer-start.sh
@@ -5,22 +5,22 @@ cd "$(dirname "$0")"
 source ../utils/lib.sh
 configure_error_handling
 
-magenta_echo "-------- SCENARIO DEPENDANT RESOURCES SETUP STAGE is starting... -------\n"
+magenta_echo "-------- SCENARIO RESOURCES SETUP STAGE is starting... -------\n"
 
 shopt -s nullglob
 
 for FOLDER in /scenario-examples/*; do
   if is_scenario_enabled "$FOLDER"; then
-    echo -e "Starting to configure scenario dependant resources from ${GREEN}$FOLDER${RESET} directory...\n\n"
+    echo -e "Starting to configure scenario resources from ${GREEN}$FOLDER${RESET} directory...\n\n"
 
     ./schema-registry/setup-schemas.sh "$FOLDER"
     ./kafka/setup-topics.sh "$FOLDER"
     ./flink/execute-flink-ddl-scripts.sh "$FOLDER"
     
-    echo -e "Scenarios dependant resources from ${GREEN}$FOLDER${RESET} directory configured!\n\n"
+    echo -e "Scenarios resources from ${GREEN}$FOLDER${RESET} directory configured!\n\n"
   else
     echo "Skipping configuring from ${GREEN}$FOLDER${RESET} directory."
   fi
 done
 
-magenta_echo "-------- SCENARIO DEPENDANT RESOURCES SETUP STAGE is finished! ---------\n\n"
+magenta_echo "-------- SCENARIO RESOURCES SETUP STAGE is finished! ---------\n\n"
